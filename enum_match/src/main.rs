d3bc7844f82583c2fa8fa83d5480138845438078
Bruno-Messias <brunocmessias@hotmail.com>
2021-05-29 21:24:31 -0300

    finishing enum and match lerning

--- enum_match/src/main.rs
+++ enum_match/src/main.rs
@@ -1,16 +1,36 @@
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+}
+
+#[derive(Debug)]
 enum Coin {
     Penny,
     Nickel,
     Dime,
-    Quarter,
+    Quarter(UsState),
 }
 
 fn value_in_cents(coin: Coin) -> u8 {
     match coin {
-        Coin::Penny => 1,
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        }
         Coin::Nickel => 5,
         Coin::Dime => 10,
-        Coin::Quarter => 25,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
+    }
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
     }
 }
 
@@ -33,6 +53,27 @@ fn main() {
 
     let absent_number: Option<i32> = None;
 
+    let wallet = Coin::Quarter(UsState::Alaska);
 
+    println!("{:?}", wallet);
+
+    let value = value_in_cents(wallet);
+
+    println!("{}", value);
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+
+    println!("{:?}", none);
+
+    let some_u8_value = 0u8; 
+    match some_u8_value { //need to 255 possibilities
+        1 => println!("one"),
+        3 => println!("three"),
+        5 => println!("five"),
+        7 => println!("seven"),
+        _ => (), // Placeholder to use as default!
+    }
 }
 

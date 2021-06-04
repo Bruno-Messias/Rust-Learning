a2f91eafb6d68b45c08b3eca1d68e18f7afa6d09
Bruno-Messias <brunocmessias@hotmail.com>
2021-06-03 21:07:37 -0300

    adding first part of study rust lang

--- error_handler/src/main.rs
+++ error_handler/src/main.rs
@@ -1,6 +1,11 @@
 use std::fs::File;
+use std::io;
+use std::fs;
+use std::io::Read;
+use std::error::Error;
 
-fn main() {
+
+fn main() -> Result<(), Box<dyn Error>> {
 
     //panic!("crash and burn");
 
@@ -11,4 +16,22 @@ fn main() {
         Err(error) => panic!("Problem opening the file: {:?}", error),
     };
 
+    //let mut f = File::open("hello.txt")?; The ? operator
+
+    let _answer = read_username_from_file();
+
+    let _f = File::open("hello.txt")?;
+
+    Ok(())
+}
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    // let mut s = String::new();
+
+    // File::open("hello.txt")?.read_to_string(&mut s)?;
+
+    // Ok(s)
+
+    fs::read_to_string("hello.txt")
+    
 }
